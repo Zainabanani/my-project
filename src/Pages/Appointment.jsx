4044b84948632951f6115142bb83aedff9a3c953
Zainabanani <mumfavouritebaby@gmail.com>
2023-07-16 13:16:55 -0700

    added another page

--- src/Pages/Appointment.jsx
+++ src/Pages/Appointment.jsx
@@ -1,8 +1,65 @@
 import React from 'react'
+import "../Styles/appoint.css";
+import logo from "../assets/Logo.png";
+import nicedoc from "../assets/nicedoc.png";
+import lines from "../assets/Lines.png";
+import Footer from "../Components/Footer"
 
 const Appointment = () => {
   return (
-    <div>Appointment</div>
+    <div className='appointment'>
+        <div>
+        <div className='headers'>
+        <nav>
+            <div className='logo'>
+                <img src={logo} alt={"logo"} />
+            </div>
+
+            <div className='maballin'>
+               <p>My Appointments</p>
+                <button className='signup'>Log Out</button>
+            </div>
+        </nav>
+    </div>
+  {/*Make an appoin */}
+  <div className='an-ap'>
+  <div className='make'>
+    <div className='shif'>
+    <div>
+    <h1>MAKE AN APPOINTMENT</h1>
+</div>
+<div>
+    <img src={lines} alt={"lines"} className='lines' />
+</div>
+<div>
+    <input type="button" placeholder='first Name*' />
+</div>
+<div>
+    <input type="button" placeholder='Last Name*' />
+</div>
+<div>
+    <input type="button" placeholder='Phone no*' />
+</div>
+<div>
+    <input type="button" placeholder='Appointment Date*' />
+</div>
+<div>
+    <button>REQUEST</button>
+</div>
+    </div>
+
+
+  </div>
+  <div className='foto'>
+    <img src={nicedoc} alt={"nicedoctor"} />
+  </div>
+  </div>
+  
+        </div>
+        <div>
+            <Footer />
+        </div>
+    </div>
   )
 }
 
